469f74f10506a2891563fbaa1c2da756385e9449
John Stevens <john.william.stevens1@gmail.com>
2020-06-05 12:15:53 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost("")]
+        public RedirectToActionResult HandleFormSubmission(string Name, string Location, string Language, string Comment)
+        {
+            return RedirectToAction("Result", new{name = Name, location = Location, language = Language, comment = Comment});
+        } 
+
+
+        [HttpGet("result")]
+        public IActionResult Result(string name, string location, string language, string comment)
+        {
+            Survey survey = new Survey(name, location, language, comment);
+            return View("Result", survey);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,18 @@
+﻿namespace DojoSurveyWithModel.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+        public string Comment { get; set; }
+
+        public Survey(string name, string location, string language, string comment)
+        {
+            Name = name;
+            Location = location;
+            Language = language;
+            Comment = comment;
+        }
+    }
+}

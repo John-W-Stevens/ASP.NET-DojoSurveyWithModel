a9b62e75f5fbd7e8f61f7edc7d2751483ed3436f
John Stevens <john.william.stevens1@gmail.com>
2020-06-05 16:08:10 -0700

    added validations

--- Models/Survey.cs
+++ Models/Survey.cs
@@ -1,18 +1,23 @@
-﻿namespace DojoSurveyWithModel.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace DojoSurveyWithModel.Models
 {
     public class Survey
     {
+
+        [Required]
+        [MinLength(2)]
         public string Name { get; set; }
+
+        [Required]
         public string Location { get; set; }
+
+        [Required]
         public string Language { get; set; }
+
+        [MaxLength(20)]
         public string Comment { get; set; }
 
-        public Survey(string name, string location, string language, string comment)
-        {
-            Name = name;
-            Location = location;
-            Language = language;
-            Comment = comment;
-        }
     }
 }
